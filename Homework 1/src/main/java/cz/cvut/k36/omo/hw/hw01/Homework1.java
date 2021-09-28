136de8000b62f4d1ac31af13097447ccbd7a4051
Vojtěch Plocica <vojta.plocica@seznam.cz>
2021-09-28 15:28:33 +0200

    Homework 1

---
+++ Homework 1/src/main/java/cz/cvut/k36/omo/hw/hw01/Homework1.java
@@ -0,0 +1,14 @@
+package cz.cvut.k36.omo.hw.hw01;
+
+public class Homework1 {
+    private int count = 0;
+    private static int staticCount = 0;
+
+    public boolean f() { return true; }
+
+    public static boolean g() { return false; }
+
+    public int h() { return ++count; }
+
+    public int i() { return ++staticCount; }
+}
